e4560ce224be17f8a200d6574d4b39ff582761e3
naga272 <114398392+naga272@users.noreply.github.com>
2023-09-19 19:39:53 +0200

    Analisi dati in C

---
+++ etl/README.md
@@ -0,0 +1,19 @@
+# ETL
+![Language](https://img.shields.io/badge/Spellcheck-Pass-green?style=flat) ![Platform](https://img.shields.io/badge/OS%20platform%20supported-Windows-blue?style=flat) ![Language](https://img.shields.io/badge/Language-Python-yellowgreen?style=flat)  ![Testing](https://img.shields.io/badge/PEP8%20CheckOnline-Passing-green) ![Testing](https://img.shields.io/badge/Test-Pass-green)
+
+## Descrizione
+programma che prende da linea di comando tre argomenti, i primi due argomenti usati come input e il terzo come output. Tutti e tre gli argomenti devono essere dei percorsi di file.
+Il programma prende i due primi argomenti passati da linea di comando e li confronta l'uno con l'altro.
+Quando una matricola non è presente in una dei due file viene inserita nel file di output (che la posizione in cui verra generato dipenderà dal percorso passato come terzo argomento)
+
+## Requisiti
+- python versione 3.11
+
+## Esecuzione
+eseguire sync.py da terminale, passando come argomento prima i due percorsi dei file csv e come terzo argomento il percorso per il file di output
+
+## Tags
+sys time argparse os re platform try except with open EXIT_SUCCESS EXIT_FAILURE ArgumentParser exit perror None void append return write localtime strftime time node environ
+
+## Author
+Bastianello Federico
---
+++ etl/bin/main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <time.h>
+
+
+#if __unix__ 		// per evitare errori a causa delle variabili d'ambiente ho deciso che il programma deve essere usato solo su unix
+	#if !defined(checker) || !defined(g_lenght)
+
+		/*		MACRO & COSTANTI	*/
+		#define 	checker(x) 		if(x) {return EXIT_FAILURE;}
+		#define 	g_length		1000				// max len x row file
+
+		/*		Prototype		*/
+		extern void 	_exit		(register int);
+		int 		logging		(void);
+		char 		*estrazione	(char[]);
+		void 		f_read		(FILE*, char[]);
+
+		/*		Struct			*/
+		typedef struct{
+			#define LEN 200				// buffer matricola
+			#define TOT_MAX_X_FILE 200 		// buffer contenitore di matricole x file
+
+			char matricola[TOT_MAX_X_FILE][LEN];	// vettore che contiene le matricole di un solo file
+							        // ogni matricola e' rappresentata tramite puntatore
+
+			int counter;				// lo uso come indice per muovermi all'interno del vettore multidimensionale matricola
+		}matricole;
+	
+	#else
+		#error, macro checker gia inizializzata
+	#endif
+#else
+	#error programma compilabile solo da s.o. unix - like
+#endif
+
+
+int main(int argc, char *argv[]){
+	setlocale(LC_CTYPE, " ");
+
+	if(logging() == EXIT_FAILURE){
+		perror("error");
+		_exit(EXIT_FAILURE);
+	}
+
+	FILE
+		*inp1 	= fopen("../flussi/77_gusers_export_alunni.txt", "r"),
+		*inp2 	= fopen("../flussi/77_SIGMA_EXPORT_ALUNNI.txt", "r"),
+		*out 	= fopen("../flussi/sync.txt", "w");
+	
+
+	checker(inp1 == NULL);
+	checker(inp2 == NULL);
+	checker(out == NULL);
+
+
+	matricole
+		xinp1 = {.counter = 0},	// contenitore delle matricole del file inp1
+		xinp2 = {.counter = 0},	// contenitore delle matricole del file inp2
+		xout;			// contenitore delle matricole del file sync
+
+
+	char line[g_length];
+	while(fgets(line, sizeof(line), inp1) != NULL){
+		strcat(xinp1.matricola[xinp1.counter], line);
+		xinp1.counter++;
+	}
+
+	return EXIT_SUCCESS;
+}
+
+
+int logging(){
+	FILE *logg = fopen("../log/trace.log", "a");	
+	checker(logg == NULL);
+
+	time_t tempo;
+	time(&tempo);
+
+	fprintf(logg, "time: %ld -- local time: %s\n", tempo, ctime(&tempo));	
+	fprintf(logg, "username: %s", getenv("USERNAME"));
+
+	fclose(logg);	
+	return EXIT_SUCCESS;
+}
+
+
+
+char *estrazione(char line[]){
+
+	return *line;
+}
